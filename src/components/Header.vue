58aa4f13e62b63e49d3840fbcaa5e5f348f307d0
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-08-26 23:55:50 +0900

    [ADD] Add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/css/style.scss";
+header {
+  height: 100vh;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="wrap">
+    <Header />
   </div>
 </template>
 
 <script>
-
+import Header from "@/components/Header";
 
 export default {
   components: {
-
+    Header,
   },
 };
 </script>
