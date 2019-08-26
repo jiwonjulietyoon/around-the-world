190b6e4b8b6865e81fc94b593c3d1c6997b927f7
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-08-26 21:54:34 +0900

    [ADD] Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <v-app>
     <router-view />
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Footer from "@/components/Footer";
 
 export default {
   name: 'App',
   components: {
-
+    Footer,
   },
   data: () => ({
     //
---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer>
+    <small>Copyright &copy; {{currentYear}} JiwonJulietYoon. All Rights Reserved</small>
+  </footer>
+</template>
+
+
+<script>
+export default {
+  name: "Footer",
+  data: () => ({
+    currentYear: 0
+  }),
+  methods: {
+    getCurrentYear() {
+      let d = new Date();
+      this.currentYear = d.getFullYear();
+    }
+  },
+  mounted() {
+    this.getCurrentYear();
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/css/style.scss";
+
+footer {
+  height: 100px;
+  background: $bg-dk-gr;
+  text-align: center;
+  color: white;
+  letter-spacing: 1px;
+}
+</style>
