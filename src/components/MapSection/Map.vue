0b3cd42e18d83244b38f5b2a15be1af0051040d9
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-08-29 18:42:58 +0900

    [MOD] Specify min/max size on world map

--- src/components/MapSection/Map.vue
+++ src/components/MapSection/Map.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <img src="" alt="">
+    <h2 class="section-title">World Map</h2>
+    <div class="world-map">
+      <img src="~@/assets/world_map.svg" alt="World Map">
+      <div class="locations">
+        
+      </div>
+    </div>
+    
   </div>
 </template>
 
@@ -18,5 +25,18 @@ export default {
 <style lang="scss" scoped>
 @import "@/css/style.scss";
 
+.world-map {
+  width: 90%;
+  max-width: 1000px;
+  @include md-down {width: 95%;}
+  @include sm-down {width: 100%;}
+  margin: 0 auto;
+  height: auto;
+  border: 1px solid hotpink;
+  position: relative;
+  & > img {
+    width: 100%;
+  }
+}
 
 </style>
